28d9f6c78c6696642e0dc282e197ea11154692f4
陈烨 <chenye@chenyedeMacBook-Pro.local>
2019-04-25 19:36:37 +0800

    :art: 完善了eslint的依赖plugin

---
+++ example.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+<script>
+export default {
+  el: 'App',
+  data() {
+    return {
+      message: 'test',
+    };
+  },
+}
+</script>
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-'use strict';
+// 'use strict';
 
 module.exports = {
-  parser: 'babel-eslint', // class properties
-  extends: ['airbnb', "plugin:vue/recommended"],
-  plugins: ["flowtype", "import", "vue"],
+  parser: 'vue-eslint-parser', // class properties
+  extends: ['airbnb', 'plugin:vue/recommended'],
+  plugins: ['flowtype', 'import', 'jsx-a11y', 'vue'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module', // es6 import/export
@@ -16,38 +16,42 @@ module.exports = {
     jest: true,
   },
   /*
-  ** "off" 或 0 - 关闭规则
+  ** 'off' 或 0 - 关闭规则
   ** "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
   ** "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
   */
   rules: {
-    "flowtype/boolean-style": "off",
-    "flowtype/delimiter-dangle": "off",
-    "flowtype/generic-spacing": "off",
-    "flowtype/object-type-delimiter": "off",
-    "flowtype/semi": "off",
-    "flowtype/space-after-type-colon": "off",
-    "flowtype/space-before-generic-bracket": "off",
-    "flowtype/space-before-type-colon": "off",
-    "flowtype/union-intersection-spacing": "off",
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止debugger
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止console
+    'no-alert': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止console
 
-    "vue/html-self-closing": 0,
-    "vue/array-bracket-spacing": "off",
-    "vue/html-closing-bracket-newline": "off",
-    "vue/html-closing-bracket-spacing": "off",
-    "vue/html-end-tags": "off",
-    "vue/html-indent": "off",
-    "vue/html-quotes": "off",
-    "vue/key-spacing": "off",
-    "vue/max-attributes-per-line": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/mustache-interpolation-spacing": "off",
-    "vue/no-multi-spaces": "off",
-    "vue/no-spaces-around-equal-signs-in-attribute": "off",
-    "vue/object-curly-spacing": "off",
-    "vue/script-indent": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/space-infix-ops": "off",
-    "vue/space-unary-ops": "off"
+    'flowtype/boolean-style': 'off',
+    'flowtype/delimiter-dangle': 'off',
+    'flowtype/generic-spacing': 'off',
+    'flowtype/object-type-delimiter': 'off',
+    'flowtype/semi': 'off',
+    'flowtype/space-after-type-colon': 'off',
+    'flowtype/space-before-generic-bracket': 'off',
+    'flowtype/space-before-type-colon': 'off',
+    'flowtype/union-intersection-spacing': 'off',
+
+    'vue/html-self-closing': 0,
+    'vue/array-bracket-spacing': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-end-tags': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-quotes': 'off',
+    'vue/key-spacing': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/mustache-interpolation-spacing': 'off',
+    'vue/no-multi-spaces': 'off',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'off',
+    'vue/object-curly-spacing': 'off',
+    'vue/script-indent': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/space-infix-ops': 'off',
+    'vue/space-unary-ops': 'off',
   },
 };
