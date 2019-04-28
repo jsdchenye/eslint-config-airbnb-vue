e6a0795cf9de81d6fa57cd1765f60a7924cd99c1
陈烨 <chenye@chenyedeMacBook-Pro.local>
2019-04-28 16:03:32 +0800

    :boom: 引入了prettier的支持

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // 'use strict';
 
 module.exports = {
-  extends: ['airbnb-base', 'plugin:vue/recommended'],
-  plugins: ['flowtype', 'import', 'vue'],
+  extends: ['airbnb-base', 'plugin:vue/recommended', 'plugin:prettier/recommended'],
+  plugins: ['import', 'vue', 'prettier'],
   parserOptions: {
     parser: 'babel-eslint', // class properties
     ecmaVersion: 2018,
@@ -16,47 +16,47 @@ module.exports = {
     jest: true,
   },
   /*
-  ** 'off' 或 0 - 关闭规则
-  ** "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
-  ** "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
-  */
+   ** 'off' 或 0 - 关闭规则
+   ** "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
+   ** "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+   */
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止debugger
     'no-console': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止console
     'no-alert': process.env.NODE_ENV === 'production' ? 2 : 0, // 打包时禁止console
 
-    'flowtype/boolean-style': 'off',
-    'flowtype/delimiter-dangle': 'off',
-    'flowtype/generic-spacing': 'off',
-    'flowtype/object-type-delimiter': 'off',
-    'flowtype/semi': 'off',
-    'flowtype/space-after-type-colon': 'off',
-    'flowtype/space-before-generic-bracket': 'off',
-    'flowtype/space-before-type-colon': 'off',
-    'flowtype/union-intersection-spacing': 'off',
-
     'vue/attribute-hyphenation': 2,
     'vue/html-closing-bracket-newline': 2,
     'vue/html-closing-bracket-spacing': 2,
     'vue/html-end-tags': 2,
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'always',
-        normal: 'always',
-        component: 'always',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
       },
-      svg: 'always',
-      math: 'always',
-    }],
-
+    ],
     'vue/max-attributes-per-line': 0,
     'vue/no-template-shadow': 2,
     'vue/singleline-html-element-content-newline': 0,
     'vue/v-bind-style': 2,
     'vue/v-on-style': 2,
-
     'vue/attributes-order': 0,
     'vue/no-v-html': 2,
     'vue/this-in-template': 2,
+
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100, // 一行的字符数，如果超过会进行换行。默认80
+        singleQuote: true, // 字符串是否使用单引号。默认false使用双引号
+        trailingComma: 'all', // 是否使用尾逗号，三个可选之’<none/es5/all>'
+      },
+    ],
   },
 };
